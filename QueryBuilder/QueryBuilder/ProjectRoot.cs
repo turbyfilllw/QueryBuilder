116d193c5a07a9a5155bbacebcfb13695b5d7f9c
turbyfilllw <turbyfilllw@etsu.edu>
2022-10-25 15:19:58 -0400

    Added connection string

---
+++ QueryBuilder/QueryBuilder/ProjectRoot.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace QueryBuilder
+{
+	public class ProjectRoot
+	{
+        public static string Root => Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+    }
+}
+
