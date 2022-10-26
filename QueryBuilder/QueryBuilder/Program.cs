817f9b1b6214cb5952fb0b5bf0bdf3617e358a4e
turbyfilllw <turbyfilllw@etsu.edu>
2022-10-26 02:09:24 -0400

    added ability to search the db for specific entry and added menu

--- QueryBuilder/QueryBuilder/Program.cs
+++ QueryBuilder/QueryBuilder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using QueryBuilder;
 using Microsoft.Data.Sqlite;
+using QueryBuilder.Models;
 
 public class Program
 {
@@ -7,6 +8,8 @@ public class Program
 
     static void Main(String[] args)
     {
+        bool menu = true;
+        int entry;
                 //Console.WriteLine(connection);
         using (var builder = new QueryBuilder.QueryBuilder(connection))
         {
@@ -17,46 +20,172 @@ public class Program
             var categories = builder.ReadAll<Categories>();
             var loans = builder.ReadAll<BooksOutOnLoan>();
 
-            Console.WriteLine("All users in the database: \n");
-            foreach(var user in users)
+            
+            while(menu)
             {
-                Console.WriteLine(user);
-                Console.WriteLine("-----------------------\n");
-            }
+                Console.WriteLine($"1. Display everything in the database\n" +
+                                  $"2. Display all users in the database\n" +
+                                  $"3. Display all books in the database\n" +
+                                  $"4. Display all authors in the database\n" +
+                                  $"5. Display all categories in the database\n" +
+                                  $"6. Display all books on loan in the database\n" +
+                                  $"7. Get a specific entry in the database\n" +
+                                  $"8. Exit");
 
-            Console.WriteLine("All books in the database: \n");
-            foreach(var book in books)
-            {
-                Console.WriteLine(book);
-                Console.WriteLine("-----------------------\n");
-            }
+                var choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
 
-            Console.WriteLine("All authors in the database: \n");
-            foreach (var author in authors)
-            {
-                Console.WriteLine(author);
-                Console.WriteLine("-----------------------\n");
-            }
+                        break;
+                    case 2:
+                        GetAllUsers(users);
+                        MoveOn();
+                        break;
+                    case 3:
+                        GetAllBooks(books);
+                        MoveOn();
+                        break;
+                    case 4:
+                        GetAllAuthors(authors);
+                        MoveOn();
+                        break;
+                    case 5:
+                        GetAllCategories(categories);
+                        MoveOn();
+                        break;
+                    case 6:
+                        GetBooksOnLoan(loans);
+                        MoveOn();
+                        break;
+                    case 7:
+                        Console.WriteLine($"Which class?\n" +
+                                          $"1. Users\n" +
+                                          $"2. Books\n" +
+                                          $"3. Authors\n" +
+                                          $"4. Categories\n" +
+                                          $"5. Books on loan\n");
 
-            Console.WriteLine("All categories in the database: \n");
-            foreach(var category in categories)
-            {
-                Console.WriteLine(category);
-                Console.WriteLine("-----------------------\n");
+                        choice = Convert.ToInt32(Console.ReadLine());
+                        switch(choice)
+                        {
+                            case 1:
+                                Console.Clear();
+                                Console.WriteLine("Which entry?: ");
+                                entry = Convert.ToInt32(Console.ReadLine());
+                                GetSpecificEntry<Users>(builder,entry);
+                                break;
+                            case 2:
+                                Console.Clear();
+                                Console.WriteLine("Which entry?: ");
+                                entry = Convert.ToInt32(Console.ReadLine());
+                                GetSpecificEntry<Books>(builder, entry);
+                                break;
+                            case 3:
+                                Console.Clear();
+                                Console.WriteLine("Which entry?: ");
+                                entry = Convert.ToInt32(Console.ReadLine());
+                                GetSpecificEntry<Author>(builder, entry);
+                                break;
+                            case 4:
+                                Console.Clear();
+                                Console.WriteLine("Which entry?: ");
+                                entry = Convert.ToInt32(Console.ReadLine());
+                                GetSpecificEntry<Categories>(builder, entry);
+                                break;
+                            case 5:
+                                Console.Clear();
+                                Console.WriteLine("Which entry?: ");
+                                entry = Convert.ToInt32(Console.ReadLine());
+                                GetSpecificEntry<BooksOutOnLoan>(builder, entry);
+                                break;
+                            default:
+                                Console.Clear();
+                                Console.WriteLine("Out of range");
+                                break;
+                        }
+                        MoveOn();
+                        break;
+                    case 8:
+                        menu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Entry");
+                        break;
+                }
             }
+            
+            //Console.WriteLine(builder.Read<Author>(1));
+            //Console.WriteLine("-----------------------\n");
 
-            Console.WriteLine("All books on loan in the database: \n");
-            foreach(var loan in loans)
-            {
-                Console.WriteLine(loan);
-                Console.WriteLine("-----------------------\n");
-            }
+            //Console.WriteLine("Deleting from database...");
+            //DeleteFromDatabase<BooksOutOnLoan>(builder);
         }
-
-        Console.ReadKey();
-        
     }
-    
+
+    private static void DeleteFromDatabase<T>(QueryBuilder.QueryBuilder builder) where T : IClassModels, new()
+    {
+        builder.Delete<T>();
+    }
+
+    private static void GetSpecificEntry<T>(QueryBuilder.QueryBuilder builder, int Id) where T : IClassModels, new()
+    {
+        
+        Console.WriteLine(builder.Read<T>(Id));
+    }
+
+    private static void GetBooksOnLoan(List<BooksOutOnLoan> loans)
+    {
+        foreach (var loan in loans)
+        {
+            Console.WriteLine(loan);
+            Console.WriteLine("-----------------------\n");
+        }
+    }
+
+    private static void GetAllCategories(List<Categories> categories)
+    {
+        foreach (var category in categories)
+        {
+            Console.WriteLine(category);
+            Console.WriteLine("-----------------------\n");
+        }
+    }
+
+    private static void GetAllAuthors(List<Author> authors)
+    {
+        foreach (var author in authors)
+        {
+            Console.WriteLine(author);
+            Console.WriteLine("-----------------------\n");
+        }
+    }
+
+    private static void GetAllBooks(List<Books> books)
+    {
+        foreach (var book in books)
+        {
+            Console.WriteLine(book);
+            Console.WriteLine("-----------------------\n");
+        }
+    }
+
+    private static void GetAllUsers(List<Users> users)
+    {
+        Console.WriteLine("-----------------------\n");
+        foreach (var user in users)
+        {
+            Console.WriteLine(user);
+            Console.WriteLine("-----------------------\n");
+        }
+    }
+
+    private static void MoveOn()
+    {
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadLine();
+        Console.Clear();
+    }
 }
 
 
--- QueryBuilder/QueryBuilder/QueryBuilder.cs
+++ QueryBuilder/QueryBuilder/QueryBuilder.cs
@@ -8,12 +8,27 @@ namespace QueryBuilder
 	{
 		private SqliteConnection connection;
 
+
+		public QueryBuilder()
+		{
+
+		}
+
+		/// <summary>
+		/// Opens the database connection
+		/// </summary>
+		/// <param name="dbPath"></param>
 		public QueryBuilder(string dbPath)
 		{
 			connection = new SqliteConnection(dbPath);
 			connection.Open();
 		}
 
+		/// <summary>
+		/// Reads all content from a database
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <returns></returns>
 		public List<T> ReadAll<T>() where T: IClassModels, new()
 		{
 			var dataString = "";
@@ -50,9 +65,60 @@ namespace QueryBuilder
 			return list;
 		}
 
-		
 
-		public void Dispose()
+		public void Create<T>(T obj) where T : IClassModels, new()
+		{
+			var command = connection.CreateCommand();
+			command.CommandText = $"INSERT INTO {typeof(T).Name}";
+		}
+		/// <summary>
+		/// Reads a specific object from the database
+		/// given an Id
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="Id"></param>
+		/// <returns></returns>
+        public T Read<T>(int Id) where T : IClassModels, new()
+        {
+			var command = connection.CreateCommand();
+			command.CommandText = $"SELECT * FROM {typeof(T).Name} WHERE Id = $id";
+
+			command.Parameters.AddWithValue("$id", Id);
+			T data = new T();
+            var reader = command.ExecuteReader();
+
+			while(reader.Read())
+			{
+				//data = new T();
+				for(int i = 0; i < reader.FieldCount; i++)
+				{
+					var propertyType = typeof(T).GetProperty(reader.GetName(i)).PropertyType;
+					var propertyName = typeof(T).GetProperty(reader.GetName(i));
+
+                    if (propertyType == typeof(int))
+                    {
+                        propertyName.SetValue(data, Convert.ToInt32(reader.GetValue(i)));
+                    }
+                    else
+                    {
+                        propertyName.SetValue(data, reader.GetValue(i));			
+                    }
+                }
+            }
+			return data;
+            
+        }
+
+		public void Delete<T>() where T : IClassModels, new()
+		{
+			var command = connection.CreateCommand();
+			command.CommandText = $"DELETE FROM {typeof(T).Name}";
+
+			command.CommandType = System.Data.CommandType.Text;
+			command.ExecuteNonQuery();
+		}
+
+        public void Dispose()
 		{
 			connection.Close();
 		}
